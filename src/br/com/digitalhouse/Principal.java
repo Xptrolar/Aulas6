1eb26438a32aeef6702f6a2a269750cd3de560d6
unknown <Digital@DH-A8-24.dhbrasil.digitalhouse.com>
2019-04-08 21:45:39 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Principal.java
@@ -0,0 +1,15 @@
+package br.com.digitalhouse;
+
+class Principal {
+
+    public  static void main(String[] args){
+
+        Cachorro cachorro = new Cachorro( "Bidu","20", "Vira-Lata");
+
+        System.out.println("Esse cachorro e mamifero ? " + cachorro.ehMamifero());
+
+        
+
+
+    }
+}
